19787bf5692ce30ec9e7640d232a0b56bc208b17
K1234 <209714565+K1234-droid@users.noreply.github.com>
2025-09-20 13:58:47 +0700

    Delete js/protocol-check.js

--- js/protocol-check.js
+++
@@ -1,5 +0,0 @@
-// This script checks if the page is being accessed via the 'file:' protocol.
-// If so, it redirects to a user-friendly error page.
-if (window.location.protocol === 'file:') {
-    window.location.href = 'FailedtoLoadPage.html';
-}
